32e41dd6b9357c6a42a97d35721a1f2b8716b58c
MythicalFlame <82922845+MythicalFlame@users.noreply.github.com>
2022-10-15 23:12:21 +0000

    Fix ban command

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -14,25 +14,31 @@ module.exports = {
         .setRequired(false)),
 	async execute (interaction) {
     const { PermissionsBitField } = require('discord.js');
+    const index = require('../index.js');
     const user = interaction.options.getUser('target');
     const member = interaction.guild.members.cache.get(user.id);
+    const client = index.botClient;
     const reason = interaction.options.getString('reason');
     //code
-    if (interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+    if (interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers, true)) {
       switch (member.id) {
         case (interaction.member.id):
           return interaction.reply('You cannot ban yourself!');
-        case (!member.bannable):
-          return interaction.reply('I cannot ban this user!');
-        case (bot.user.id):
+        case (client.user.id):
           return interaction.reply('You cannot ban the bot!');
-        case (member.roles.highest.position >= interaction.member.roles.highest.position):
-          return interaction.reply('You cannot ban someone higher in the role hierarchy!');
         default:
-          await interaction.reply(`${member} has been banned from the server for ${reason}`);
-          await member.ban({reason: `${interaction.user} has stated the reason: ${reason}`});
+          if (!member.bannable) {
+            await interaction.reply('I cannot ban this user!');
+          } else if (member.roles.highest.position >= interaction.member.roles.highest.position) {
+            await interaction.reply('You cannot ban someone higher than or equal to you in the role hierarchy!')
+          } else {
+            await interaction.reply(`${member} has been banned from the server for ${reason}`);
+            await member.ban({reason: `${interaction.user} has stated the reason: ${reason}`});
+          }
           break;
       }
+    } else {
+      await interaction.reply('You do not have ban permissions!');
     }
 	},
 };
